f3a1750ee4af5657f2fe2acb52416b78e2e9a60f
mabergerx <maberger0811@gmail.com>
2019-10-08 16:13:38 +0200

    first wrangling functions

---
+++ data_wrangling.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+all_performances = pd.read_csv("data/all_performances_preprocessed_v6.csv", sep=";")
+
+def give_location_events(data, year=None, top_n=20):
+    if year:
+        location_events = data[(data['geocode'] != "-") & (data["year"] == year)].groupby('geocode')['programID'].nunique().reset_index(
+            name="count").sort_values(by="count", ascending=True).tail(top_n)
+    else:
+        location_events = data[data['geocode'] != "-"].groupby('geocode')[
+            'programID'].nunique().reset_index(
+            name="count").sort_values(by="count", ascending=True).tail(top_n)
+    return location_events
+
+
+def give_sub_df(data, year, location, composerName=None, eventType=None, conductorName=None,
+                workTitle=None, soloistName=None, orchestra=None, seasonOfYear=None):
+
+
+    return data[(data.Country == location) & (data.year == year)]
+
+
