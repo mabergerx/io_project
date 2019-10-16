f6480b1db6252936729360e42f390ed7d6acc5fa
mabergerx <maberger0811@gmail.com>
2019-10-16 16:08:23 +0200

    debug mode to False

--- run_website.py
+++ run_website.py
@@ -1162,4 +1162,4 @@ if __name__ == '__main__':
     big_location_aggregation = dw.read_country_data().sort_values("year").reset_index(drop=True)
     big_location_aggregation_state = dw.read_state_data().sort_values("year").reset_index(drop=True)
     all_performances = dw.read_all_performances().sort_values("year").reset_index(drop=True)
-    app.run_server(debug=True, port=5000, host='0.0.0.0')
+    app.run_server(debug=False, port=5000, host='0.0.0.0')
